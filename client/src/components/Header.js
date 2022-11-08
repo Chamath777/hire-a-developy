a4402e50491c457522db96b2173083c9efdcfe7b
MarkBrooks1985 <alynnnari@gmail.com>
2022-11-08 21:40:30 +1030

    another update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,11 +3,16 @@ import { useMutation } from "@apollo/react-hooks";
 
 import { Link } from "react-router-dom";
 
+import Splash from "../assets/images/splash.png";
+
 export default function Header() {
   return (
     <header className="bg-slate-500 border-b-2 border-solid border-slate-100 py-5 fixed top-0">
       <span className="text-white text-2xl">
-        <img src="https://via.placeholder.com/1900x300" />
+        <img
+          className="border-dashed rounded border-white border-2"
+          src={Splash}
+        />
       </span>
     </header>
   );
