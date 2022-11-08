f563baba75d3631090074e51f95b55f05526ce4c
MarkBrooks1985 <alynnnari@gmail.com>
2022-11-08 21:35:11 +1030

    another update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-slate-500 border-b-2 border-solid border-slate-100 py-5">
+    <header className="bg-slate-500 border-b-2 border-solid border-slate-100 py-5 fixed top-0">
       <span className="text-white text-2xl">
-        <img src="https://via.placeholder.com/1900x100" />
+        <img src="https://via.placeholder.com/1900x300" />
       </span>
     </header>
   );
