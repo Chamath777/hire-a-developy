5ff0d792957c288fb74bd7cb40eee97b9ef786fe
MarkBrooks1985 <alynnnari@gmail.com>
2022-11-08 21:51:31 +1030

    footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="p-4 bg-slate-500 md:flex md:items-center md:justify-between md:p-6 fixed bottom-0 w-full">
+    <footer className="p-4 bg-slate-500 md:flex md:items-center md:justify-between md:p-6 fixed bottom-0 w-full border-solid border-t-2 border-white">
       <span className="text-sm text-white sm:text-center">
         © 2022{" "}
         <a href="https://github.com/Chamath777" className="hover:underline">
