e91b09891ccc6058d4e1a058aa7ea112804cfdf6
MarkBrooks1985 <alynnnari@gmail.com>
2022-11-15 17:15:05 +1030

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://StarlitOne:606Ellessia606@cluster0.59zbscx.mongodb.net/blooming-peak-18055?retryWrites=true&w=majority",
+    "mongodb+srv://StarlitOne:606Ellessia606@blooming-peak.18055.mongodb.net?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
