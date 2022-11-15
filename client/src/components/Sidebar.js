bd8d69ad613927a71b1de51dee9546fd36d2cd78
MarkBrooks1985 <alynnnari@gmail.com>
2022-11-15 17:22:25 +1030

    more fixes

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -23,6 +23,7 @@ export default function Sidebar() {
           <li>
             <a
               href="/"
+              alt="this is a home link"
               className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
             >
               <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
@@ -37,6 +38,7 @@ export default function Sidebar() {
                 <a
                   href="/logout"
                   onClick={logout}
+                  alt="this is a logout link"
                   className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
                 >
                   <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
@@ -51,6 +53,7 @@ export default function Sidebar() {
               <li>
                 <a
                   href="/login"
+                  alt="this is a login link"
                   className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
                 >
                   <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
@@ -62,6 +65,7 @@ export default function Sidebar() {
               <li>
                 <a
                   href="/signup"
+                  alt="this is a signup link"
                   className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
                 >
                   <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
@@ -75,6 +79,7 @@ export default function Sidebar() {
           <li>
             <a
               href="/Developer"
+              alt="this is a developer link"
               className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
             >
               <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
@@ -86,6 +91,7 @@ export default function Sidebar() {
           <li>
             <a
               href="/Products"
+              alt="this is a products link"
               className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800"
             >
               <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white">
